caa79a3902962800360ad0ad469aed094f81541c
ALS_here <ALEX.SMITH@STONEGROUP.CO.UK>
2020-10-26 20:50:39 +0000

    registration page with model (nearly complete)

--- src/Models/Register.php
+++ src/Models/Register.php
@@ -3,9 +3,12 @@ namespace App\Models;
 
 use App\Helpers\Config;
 use App\Helpers\Logger;
+use App\Models\AbstractModel;
 
 class Register extends AbstractModel
 {
+
+    public $user;
     /**
      * Register constructor.
      */
@@ -21,7 +24,198 @@ class Register extends AbstractModel
         /**
          * @todo form submited
          */
+        $values = array();
 
+        try {
+            $email = stripslashes(strtolower($_POST['email']));
+            $this->loadByEmail($email);
+
+            // Check if user already exists or not
+            if (!$this->user) {
+                // Build up array from posted values
+                // foreach ($_POST as $k => $v) {
+                //     if ($k != "customer_id") {
+                //         $values[':' . $k] = stripslashes($v);
+                //     }
+                // }
+
+ 
+
+                // Create record for user in recyc_users return the new user id
+                $sql = "INSERT INTO recyc_users (username, firstname,lastname, email, password, active, role_id, CompanyNUM, telephone, number_type, position, `email_preferences`, `post_preferences`, `phone_preferences`) VALUES (:username,:firstname, :lastname,:email, 1, 3, :compnum, :telephone, :number_type, :number_type, :position, 'N', 'N', 'N')";
+                $result = $this->rdb->prepare($sql);
+                $result->execute(array(':username' => $_POST['username'], 
+                ':firstname' => $_POST['firstname'], 
+                ':lastname' => $_POST['lastname'],
+                ':email' => $_POST['email'],
+                ':compnum' => $_POST['compnum'],
+                ':telephone' => $_POST['telephone'],
+                ':number_type' => $_POST['number_type'],
+                ':position' => $_POST['position'],
+            ));
+
+                ///add to company list
+                $sql = 'INSERT INTO `recyc_company_list` ( `company_name`, `portal_requirement`, `assigned_to_bdm`, `cod_required`, `amr_required`, `rebate_required`, `remarketingrep_required`, `blancco_required`, `manual_customer`, `reference_code`, `reference_source`) VALUES(:companyname, 0, NULL, 0, 0, 0, 0, 0, 0, NULL, NULL)';
+                $result = $this->rdb->prepare($sql);
+                $result->execute(array(':companyname' => $_POST['companyname']));
+
+                // Get user id just created and init $this->user to stop empty object warning message
+                $this->user = new \stdClass();
+                $this->user->id = $this->rdb->lastInsertId();
+
+                // Check if a user id was returned by the insert
+                if ($this->user->id != 0) {
+                    // Change tag: addCustomer
+                    // Check if adding a user with a customer
+
+                        // Add each custome to the user
+                        //foreach($_POST["customer_id"] as $customer_id) {
+                        // Insert record
+                        // $sql = "INSERT INTO recyc_customer_links_to_company (user_id, company_id) VALUES (:id,:customer_id)";
+                        // $statement = $this->rdb->prepare($sql);
+                        // $values = array(':id' => $this->user->id, ':customer_id' => $_POST["customer_id"]);
+                        // $statement->execute($values);
+                        //}
+                    
+                    // End change tag addCustomer
+
+                    // Generate token and send password change email
+                    //$this->get($this->user->id);
+                    $this->generateToken();
+                   // $this->sendReminder("new");
+                } else {
+                    // Show error
+
+                    $this->response = '
+					<div class="alert alert-danger fade-in" id="reset-container" >
+						<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+						<h4>Error</h4>
+						<p>Unable to save user due to a bad data array.</p>
+					</div>
+					';
+                }
+
+                $this->response = '
+				<div class="alert alert-success fade-in" id="reset-container" >
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+					<h4>Profile Updated</h4>
+					<p>User updated successfully</p>
+				</div>
+				';
+            } else {
+                $this->response = '
+				<div class="alert alert-danger fade-in" id="reset-container" >
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+					<h4>Error</h4>
+					<p>A user with this email address already exists.</p>
+				</div>
+				';
+            }
+        } catch (\Exception $e) {
+            $this->response = '
+			<div class="alert alert-danger fade-in" id="reset-container" >
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+				<h4>Error</h4>
+				<p>A user with this email address already exists.</p>
+				<p>' . $e->getMessage() . '</p>
+			</div>
+			';
+        }
         return ['success' => true];
     }
+
+    public function loadByEmail($email)
+    {
+
+        $email = strtolower($email);
+
+        if ($email) {
+            $sql = 'SELECT * FROM recyc_users WHERE email = :email';
+            $result = $this->rdb->prepare($sql);
+            $result->execute(array(':email' => $email));
+            $this->user = $result->fetch(\PDO::FETCH_OBJ);
+        }
+    }
+
+    public function generateToken()
+    {
+        $length = 32;
+        $this->token = bin2hex(random_bytes($length));
+        $expires = new \DateTime();
+        $expires->modify('+2 days');
+        $this->expiry = $expires->format('Y-m-d H:i:s');
+
+        $sql = 'UPDATE recyc_users SET token = :token, token_expires = :expiry WHERE id = :id';
+        $values = array(':token' => $this->token, ':expiry' => $this->expiry, ':id' => $this->user->id);
+        $result = $this->rdb->prepare($sql);
+        $result->execute($values);
+    }
+
+    public function get($id = false)
+    {
+
+        if ($id) {
+            $sql = 'SELECT
+				id,
+				username,
+				firstname,
+				lastname,
+				email,
+				active,
+				role_id,
+				password
+			FROM
+				recyc_users
+			WHERE
+				id = :id
+			';
+            $result = $this->rdb->prepare($sql);
+            $result->execute(array(':id' => $id));
+            $this->user = $result->fetch(\PDO::FETCH_OBJ);
+
+            // $sql   = 'SELECT structure_id FROM recyc_permissions WHERE role_id = :role';
+            // $result = $this->rdb->prepare( $sql );
+            // $result->execute(array(':role' => $this->user->role_id));
+            // $this->user->permissions = $result->fetchAll(\PDO::FETCH_OBJ );
+
+            $sql = 'SELECT
+				c.id,
+				c.company_name
+			FROM
+				recyc_customer_links_to_company cc
+			left join
+				recyc_company_list c on cc.company_id = c.id
+			where
+				user_id = :id
+			';
+            $result = $this->rdb->prepare($sql);
+            $result->execute(array(':id' => $id));
+            $this->user->companies = $result->fetchAll(\PDO::FETCH_OBJ);
+
+            $sql = 'SELECT
+				c.id,
+				c.company_name
+			from
+				recyc_customer_links_to_company cc
+			left join
+				recyc_company_list c on cc.company_id = c.id
+			where
+				user_id = :id
+			';
+            $result = $this->rdb->prepare($sql);
+            $result->execute(array(':id' => $id));
+            $this->user->customers = $result->fetchAll(\PDO::FETCH_OBJ);
+        } else {
+            $sql = 'SELECT * FROM recyc_users WHERE (role_id = 1 OR role_id = 2)';
+            $result = $this->rdb->prepare($sql);
+            $result->execute();
+            $this->stoneUsers = $result->fetchAll(\PDO::FETCH_OBJ);
+
+            $sql = 'SELECT * FROM recyc_users WHERE (role_id = 3 OR role_id = 4)"';
+            $result = $this->rdb->prepare($sql);
+            $result->execute();
+            $this->users = $result->fetchAll(\PDO::FETCH_OBJ);
+        }
+    }
+
 }
--- views/template/register/register-form.php
+++ views/template/register/register-form.php
@@ -4,9 +4,25 @@
 
 <script type="text/javascript">
     jQuery(document).ready(function () {
-        jQuery('#login').on('click', function () {
+        $('#part2').hide();
+
+
+        jQuery('#next').on('click', function () {
+             $('#part2').show();
+
+            $('#part1').hide();
+         
+        });
+        jQuery('#reg').on('click', function () {
             password = jQuery('#password').val();
             username = jQuery('#username').val();
+            email = jQuery('#username').val();
+            companyname = jQuery('#companyname').val();
+            compnum = jQuery('#compnum').val();
+            comptype = jQuery('#comptype option:selected').val();
+            firsname = jQuery('#firsname').val();
+            lastname = jQuery('#lastname').val();
+            telephone = jQuery('#lastname').val();
             redirect = "<?php echo $redirect; ?>";
 
             if (username.length < 3) {
@@ -15,11 +31,37 @@
             } else if (password.length < 3) {
                 alert('Please enter your password');
                 jQuery('#password').focus();
-            } else {
+            } else if (lastname.length < 3) {
+                alert('Please enter your password');
+                jQuery('#password').focus();
+            }else if (firsname.length < 3) {
+                alert('Please enter your password');
+                jQuery('#password').focus();
+            } else if (compnum.length < 3) {
+                alert('Please enter your company number');
+                jQuery('#password').focus();
+            }  else if (companyname.length < 3) {
+                alert('Please enter your company name');
+                jQuery('#password').focus();
+            }
+            else {
                 jQuery.ajax({
                     url: "/register/register",
                     type: "POST",
-                    data: "username=" + username + "&password=" + password + "&redirect=" + redirect,
+                   // data: "username=" + username + "&password=" + password + "&redirect=" + redirect,
+                   data : {
+
+                       username : username, 
+                       firstname : firsname,
+                       lastname : lastname, 
+                       companyname : companyname,
+                       email : email, 
+                       password : password,
+                       compnum :compnum, 
+                       telephone : telephone,
+                       comptype : comptype
+
+                   },
                     success: function (data) {
                         if (data == 1) {
                             document.location = '<?php echo $redirect; ?>'
@@ -36,4 +78,54 @@
 
 
 <h2>REGISTER FORM</h2>
+<div class="row">
+    <form class="form-signin">
+        <h2 class="form-signin-heading">Register Here</h2>
+     
+        <form name='registration' method="">
+    <div id='part1'>
+  <div class="form-group">
+    <label for="companyname">Company Name</label>
+    <input type="text" class="form-control" id="companyname" placeholder="Company Name">
+    <label for="companyname">First Name</label>
+    <input type="text" class="form-control" id="firstname" placeholder="First Name">
+    <label for="companyname">Last Name</label>
+    <input type="text" class="form-control" id="lastname" placeholder="Last Name">
+    <label for="companyname">Telephone</label>
+    <input type="text" class="form-control" id="telephone" placeholder="Telephone">
+    <label for="companyname">Position</label>
+    <input type="text" class="form-control" id="Position" placeholder="Position">
+  </div>
+  <div class="form-group">
+    <label for="comptype">My company is part of the...</label>
+    <select class="form-control" id="comptype">
+      <option value="CH">Private</option>
+      <option value="URN">Public</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="compnum">Company Number</label>
+    <input type="text" class="form-control" id="compnum" placeholder="Company Number">
+  </div> 
+  <a  id='next' class='btn btn-success'>Next </a>
+  </div>
+ 
+
+  <div id='part2'>
+  <div class="form-group">
+    <label for="username">Email Address</label>
+    <input type="email" class="form-control" id="username" placeholder="enter email here">
+  </div>
+  <div class="form-group">
+    <label for="password">Password</label>
+    <input type="password" class="form-control" id="password" placeholder="password">
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" id="reg">Submit</button>
+
+
+</div>
+  
+  
+</form>
+</div>
 
