84776bb1134754e9b59ab7fc070bbe2cc34f8fec
webulent <me@bulentkocaman.com>
2020-10-27 12:10:57 +0000

    added email validation

--- src/Models/Register.php
+++ src/Models/Register.php
@@ -1,29 +1,41 @@
 <?php
+
 namespace App\Models;
 
-use App\Helpers\Config;
 use App\Helpers\Logger;
-use App\Models\AbstractModel;
 
+/**
+ * Class Register
+ * @package App\Models
+ */
 class Register extends AbstractModel
 {
-
     public $user;
+
     /**
      * Register constructor.
      */
     public function __construct()
     {
-        $this->emailConfig = Config::getInstance()->get('email');
-
         parent::__construct();
     }
 
     public function register()
     {
         try {
-            $email = stripslashes(strtolower($_POST['email']));
-            $this->loadByEmail($email);
+            $email = filter_var(stripslashes(strtolower($_POST['email'])), FILTER_SANITIZE_EMAIL);
+
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $this->loadByEmail($email);
+            } else {
+                $message = '
+				<div class="alert alert-danger fade-in" id="reset-container" >
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+					<h4>Error</h4>
+					<p>E-mail address is not valid.</p>
+				</div>';
+                return ['success' => false, 'message' => $message];
+            }
 
             if (!$this->user) {
                 $sql = "INSERT INTO recyc_users (username, firstname,lastname, email, password, active, role_id, CompanyNUM, telephone, number_type, position, `email_preferences`, `post_preferences`, `phone_preferences`) 
@@ -31,10 +43,10 @@ class Register extends AbstractModel
                 $result = $this->rdb->prepare($sql);
                 $result->execute(
                     [
-                        ':username' => filter_var($_POST['username'], FILTER_SANITIZE_STRING),
+                        ':username' => $email,
                         ':firstname' => filter_var($_POST['firstname'], FILTER_SANITIZE_STRING),
                         ':lastname' => filter_var($_POST['lastname'], FILTER_SANITIZE_STRING),
-                        ':email' => filter_var($_POST['email'], FILTER_SANITIZE_STRING),
+                        ':email' => $email,
                         ':compnum' => filter_var($_POST['compnum'], FILTER_SANITIZE_STRING),
                         ':telephone' => filter_var($_POST['telephone'], FILTER_SANITIZE_STRING),
                         ':number_type' => filter_var($_POST['comptype'], FILTER_SANITIZE_STRING),
@@ -44,13 +56,11 @@ class Register extends AbstractModel
                 );
                 $this->user = new \stdClass();
                 $this->user->id = $this->rdb->lastInsertId();
+
                 ///add to company list
                 $sql = 'INSERT INTO `recyc_company_list` ( `company_name`, `portal_requirement`, `assigned_to_bdm`, `cod_required`, `amr_required`, `rebate_required`, `remarketingrep_required`, `blancco_required`, `manual_customer`, `reference_code`, `reference_source`) VALUES(:companyname, 0, NULL, 0, 0, 0, 0, 0, 0, NULL, NULL)';
                 $result = $this->rdb->prepare($sql);
                 $result->execute(array(':companyname' => $_POST['companyname']));
-
-                // Get user id just created and init $this->user to stop empty object warning message
-
 
                 // Check if a user id was returned by the insert
                 if ($this->user->id) {
@@ -59,20 +69,19 @@ class Register extends AbstractModel
                     <div class="alert alert-success fade-in" id="reset-container" >
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
                         <h4>Success</h4>
-                        <p>thank you for registering.</p>
+                        <p>Thank you for registering.</p>
                     </div>
                     ';
-                    return ['success' => true,  'message' => $message];
-
+                    return ['success' => true, 'message' => $message];
                 } else {
                     $message = '
 					<div class="alert alert-danger fade-in" id="reset-container" >
 						<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
 						<h4>Error</h4>
-						<p>Unable to save user due to a bad data array.</p>
+						<p>Unable to save user.</p>
 					</div>
 					';
-                    return ['success' => false,  'message' => $message];
+                    return ['success' => false, 'message' => $message];
                 }
             } else {
                 $message = '
@@ -80,23 +89,27 @@ class Register extends AbstractModel
 					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
 					<h4>Error</h4>
 					<p>A user with this email address already exists.</p>
+				    <p><a href="/login">Login</a></p>
 				</div>
 				';
-                return ['success' => false,  'message' => $message];
+                return ['success' => false, 'message' => $message];
             }
         } catch (\Exception $e) {
+            Logger::getInstance("Register.log")->error(
+                'failed',
+                ['line' => $e->getLine(), 'error' => $e->getMessage()]
+            );
             $message = '
 			<div class="alert alert-danger fade-in" id="reset-container" >
 				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-				<h4>Error</h4>
-				<p>A user with this email address already exists.</p>
-				<p>' . $e->getMessage() . '</p>
+				<h4>ERROR!</h4>
+				<p>Can not complete the process.</p>
 			</div>
 			';
-            return ['success' => false,  'message' => $message];
+            return ['success' => false, 'message' => $message];
         }
 
-        return ['success' => false,  'message' => 'Unable to complete process.'];
+        return ['success' => false, 'message' => 'Unable to complete process.'];
     }
 
     public function loadByEmail($email)
@@ -122,6 +135,15 @@ class Register extends AbstractModel
         $values = array(':token' => $this->token, ':expiry' => $this->expiry, ':id' => $this->user->id);
         $result = $this->rdb->prepare($sql);
         $result->execute($values);
+    }
+
+    private function passValidation($pass)
+    {
+        if (strlen($pass) > 20 ||
+            strlen($pass) < 5) {
+            return false;
+        }
+        return true;
     }
 
 }
