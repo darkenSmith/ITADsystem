5b0e89b7ad70951c6befec95313147c03a8a3d3a
Alex Smith <Alex.Smith@stonegroup.co.uk>
2020-11-03 16:53:44 +0000

    changes to helper and ini

--- src/Helpers/CompanyHouseApi.php
+++ src/Helpers/CompanyHouseApi.php
@@ -47,7 +47,6 @@ class CompanyHouseApi
 
         try {
             $client = new Client();
-            
 
             $headers = [
                 'Authorization' => "Basic " . base64_encode($this->apiConf['api']['key'] . ":"),
@@ -56,9 +55,8 @@ class CompanyHouseApi
             $response = $client->request(
                 'GET',
                 sprintf('%s%s%s', $this->apiConf['api']['url'], $endpoint, $parameter),
-                [
-                    'headers' => $headers,
-                    'verify' => false
+                ['headers' => $headers,
+                'verify' => $this->apiConf['ssl']['verify'],
                 ]
             );
 
